c28427bc1e4ce5cb6d03c8910aec1af3ab5cf3db
car-rawgo <iankarugu1@gmail.com>
2024-04-10 14:13:34 +0300

    added `week 2.py`

---
+++ week 2.py
@@ -0,0 +1,20 @@
+my_list = []
+
+print(my_list)
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list = [10,20,30,40]
+my_list[1] = 15
+print(my_list)
+numbers = [50,60,70]
+my_list.extend(numbers)
+print(my_list)
+del my_list[-1]
+print(my_list)
+my_list.sort()
+print(my_list)
+print(my_list.index(30))
+   
